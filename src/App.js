db1d14d81ca3c2c7bdfc201c68548f15d0eafb33
Samer Khatib <skhatib07@outlook.com>
2021-07-26 17:29:01 -0400

    Successfully launched website on own DNS. This is the current src code for the website at samerkhatib.com

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nice.
+          Welcome to my Personal Portfolio Website! <br/>
+          This Website is Currently a Work in Progress.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am Learning React
         </a>
       </header>
     </div>
